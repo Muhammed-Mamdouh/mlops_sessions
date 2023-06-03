1c396728e1543659b2427921f6093c9ffe27df61
Mand0r0ck3r <muhmamsal@gmail.com>
2023-06-03 14:45:04 +0300

    add another end point

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
 from fastapi import FastAPI
+import logging
+from hello import sum_xy
+from data_models import SamplePostRequest
+
+
+log_format = "[%(name)s][%(levelname)-6s] %(message)s"
+logging.basicConfig(format=log_format)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 app = FastAPI()
 
 @app.get("/home")
 def home():
     return "hello"
+
+
+@app.get("/sum/{c}")
+def sum_xy_endpoint(c: str, a: int, b: int):
+    logger.info("Recievevd request.")
+
+    try:
+        sum_xy("1", 7)
+    except:
+        logger.error("NOOOOOOOOOOOOOOOOOOOOOOOOOO")
+
+    output = c + " " + str(sum_xy(a, b))
+
+    return output
+
+
+@app.post("/predict")
+def predict(request: SamplePostRequest):
+
+    return [request.a + request.b]
--- tests/test_app.py
+++ tests/test_app.py
@@ -8,3 +8,9 @@ client = TestClient(app)
 def test_home():
     response = client.get("/home")
     assert response.status_code == 200
+
+
+def test_predict():
+    response = client.post("/predict", json={"a":7, "b":5})
+    assert response.status_code == 200
+    print(response.content)
