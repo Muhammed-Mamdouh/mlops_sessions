6696915f00b14a7f18d289c6c74b7e23659c8068
Mand0r0ck3r <muhmamsal@gmail.com>
2023-06-03 11:38:11 +0300

    wrong test2

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 """hello"""
+
+
 def sum_xy(value_1, value_2):
     """docstring"""
     return value_1 + value_2
 
 
-if __name__=="__main__":
-    print(sum_xy(1,2))
+if __name__ == "__main__":
+    print(sum_xy(1, 2))
--- test_hello.py
+++ test_hello.py
@@ -3,4 +3,4 @@ from hello import sum_xy
 
 def test_sum_xy():
     assert 2 == sum_xy(1, 1)
-    assert 6 == sum_xy(3,1 )
+    assert 6 == sum_xy(3,1)
